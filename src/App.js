b6fb69ac33eb91bb525486004c29a5558c603a04
BirdieOA <maitedeleonf@gmail.com>
2024-01-02 20:21:08 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import imagen0 from "./images/rey_ataulfo.png";
+import imagen1 from "./images/rey_sisebuto.png";
+import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const cambio = 23.16;
+  const refCaja = useRef();
+  function incrementar(e) {
+    e.target.innerHTML = Number(e.target.innerHTML) + 1;
+  }
+  //  e.target.style.backgroundColor="peru";
+  const convertir=()=> {
+    refCaja.current.innerHTML = Number(refCaja.current.innerHTML) * cambio;
+  }
+
+  const cambiar=(j)=> {
+    j.target.src = imagen1;
+  }
+  const lectura=(e)=>{
+    refCaja.current.innerHTML=e.target.value;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div ref={refCaja} className="caja" onClick={incrementar}>
+        1
+      </div>
+      <button onClick={convertir}>Aceptar</button>
+      <div>
+        <img onClick={cambiar} src={imagen0}/>
+      </div>
+      <input onChange={lectura} className="campo" />
+    </>
   );
 }
 
 export default App;
+
+//target es para decirme a que elemento le he hecho el envento onClick
+//simpre que se use un useRef que es para referenciar un elemento seguido de el va un current
